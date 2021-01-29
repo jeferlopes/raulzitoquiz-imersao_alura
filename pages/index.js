eb81171c2e9cf35ef4269a1969b0336b5dd8221c
jeferlopes <jefersonrodrigolopes@gmail.com>
2021-01-28 22:05:23 -0300

    codigo ao final da segunda aula da imersão react alura

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import QuizLogo from '../src/components/QuizLogo'
 import QuizBackground from '../src/components/QuizBackground'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
 
 const BackgroundImage = styled.div`
   background-image: url(${db.bg});
@@ -25,8 +27,35 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+  console.log('retorno do useState', name, setName);
+
     return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+      <title>Raulzito Quiz</title>
+      <meta name="title" content="Raulzito Quiz" />
+      <meta name="description" content="Sabe tudo sobre o grande Raul Seixas?
+      Será mesmo?
+      Encare o nosso Quiz, e mostre que você é a Mosca na Sopa, e viva a Sociedade Alternativa!" />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://metatags.io/" />
+      <meta property="og:title" content="Raulzito Quiz" />
+      <meta property="og:description" content="Sabe tudo sobre o grande Raul Seixas?
+      Será mesmo?
+      Encare o nosso Quiz, e mostre que você é a Mosca na Sopa, e viva a Sociedade Alternativa!" />
+      <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://metatags.io/" />
+      <meta property="twitter:title" content="Raulzito Quiz" />
+      <meta property="twitter:description" content="Sabe tudo sobre o grande Raul Seixas?
+      Será mesmo?
+      Encare o nosso Quiz, e mostre que você é a Mosca na Sopa, e viva a Sociedade Alternativa!" />
+      <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+      </Head>
       <QuizContainer>
         <QuizLogo />
         <Widget>
@@ -37,14 +66,36 @@ export default function Home() {
             <p>{db.description}</p>
           </Widget.Content>
         </Widget>
-
         <Widget>
+          <Widget.Content>
+            <form onSubmit={function (infosdoEvento) {
+                infosdoEvento.preventDefault();
+                router.push(`/quiz?name=${name}`);
+                console.log('Fazendo uma submit por meio do react');
+            }}
+            >
+              <input
+                onChange={function (infosdoEvento) {
+                  console.log(infosdoEvento.target.value);
+
+                  // name = infosdoEvento.target.value;
+                  setName(infosdoEvento.target.value);
+                }}
+              
+              placeholder="Ei! Quem é você?" />
+              <button type="submit" disabled={name.length === 0}> 
+                Vamos! {name} Responda!
+              </button>
+            </form>
+          </Widget.Content>
+        </Widget>
+        {/* <Widget> */}
           {/* <Widget.Content>
             <h1>Quizes da Galera</h1>
 
             <p>lorem ipsum dolor sit amet...</p>
           </Widget.Content> */}
-        </Widget>
+        {/* </Widget> */}
         <Footer />
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/jeferlopes" />
